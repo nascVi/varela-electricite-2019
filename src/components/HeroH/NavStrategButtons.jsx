c6bd714c3a4f300d7f618a9aead4911a702699ed
nascVi <vaiiho.nascimbene@egmail.com>
2019-03-22 00:12:18 +0100

    define hero buttons

--- src/components/HeroH/NavStrategButtons.jsx
+++ src/components/HeroH/NavStrategButtons.jsx
@@ -3,8 +3,15 @@ import React from 'react';
 const NavStrategButtons = () => {
   /* svg Design des button et annimations onClick, OnMouseover, onMouseleave(dzzit effect)
   check for pointer work*/
+  const LeftButton = () {return(<button OnMouseover={stylus}>Nous contacter</button>)}
+  const RightButton = () {return(<button OnMouseover={stylus}>Découvrir</button>)}
+
   return (
-    <div></div>
+    <div>
+
+    <LeftButton onClick={rediredtoSContacts} />
+    <RightButton onClick={rediredtoSIntro} />
+    </div>
   )
 }
 
