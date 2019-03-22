3148f73bdfdf277c766ff4a1b158176559719eb8
nascVi <vaiiho.nascimbene@gmail.com>
2019-03-23 00:15:58 +0100

    initial background and buttons for hero

--- src/components/HeroH/NavStrategButtons.jsx
+++ src/components/HeroH/NavStrategButtons.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
+// import { section } from 'style.css'
 
 const NavStrategButtons = () => {
   /* svg Design des button et annimations onClick, OnMouseover, onMouseleave(dzzit effect)
   check for pointer work*/
   return (
-    <div></div>
+    <div>
+    <section class="section section--menu" id="Adrian">
+				<h2 class="section__title">Adrian</h2>
+				<span class="link-copy"></span>
+				<nav class="menu menu--adrian">
+					<ul class="menu__list">
+						<li class="menu__item menu__item--current"><a href="/" class="menu__link"><span class="menu__helper">Home</span></a></li>
+						<li class="menu__item"><a href="/" class="menu__link"><span class="menu__helper">Recipes</span></a></li>
+					</ul>
+				</nav>
+			</section>
+    </div>
   )
 }
 
--- src/containers/SHero/SHero.jsx
+++ src/containers/SHero/SHero.jsx
@@ -1,13 +1,17 @@
 import React, {/* useState */} from 'react'
 import FadeInLogo from '../../components/HeroH/FadeinLogo'
+import NavStrategButtons from '../../components/HeroH/NavStrategButtons'
 
 const Hero = () => {
   
  // Afficher logo avec effet de fondu à ouverture
   return (
-    <div>
+    <div className="">
 		<h1>I am the hero here!</h1>
-		<FadeInLogo />
+    <FadeInLogo />
+    {/* Following section is using the onLoad method, displaying the function ready strategButtons
+     the adrian way*/}
+    <NavStrategButtons />
     </div>
   )
 }
