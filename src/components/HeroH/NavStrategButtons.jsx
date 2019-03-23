8b028a33106d37b09cce60c3e68375e3a417fc29
nascVi <vaiiho.nascimbene@gmail.com>
2019-03-23 18:20:45 +0100

    buttons miscCcode

--- src/components/HeroH/NavStrategButtons.jsx
+++ src/components/HeroH/NavStrategButtons.jsx
@@ -4,8 +4,8 @@ import React from 'react';
 const NavStrategButtons = () => {
   /* svg Design des button et annimations onClick, OnMouseover, onMouseleave(dzzit effect)
   check for pointer work*/
-  const LeftButton = () {return(<button OnMouseover={stylus}>Nous contacter</button>)}
-  const RightButton = () {return(<button OnMouseover={stylus}>Découvrir</button>)}
+  // const LeftButton = () {return(<button OnMouseover={stylus}>Nous contacter</button>)}
+  // const RightButton = () {return(<button OnMouseover={stylus}>Découvrir</button>)}
 
   return (
     <div>
