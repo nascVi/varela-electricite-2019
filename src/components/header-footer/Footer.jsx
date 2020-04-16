621017d7d4eb79530468a8cf3363b22690ba4fbf
nascVi <vaiiho.nascimbene@gmail.com>
2020-04-16 15:35:43 +0200

    more footer design and social network icons

--- src/components/header-footer/Footer.jsx
+++ src/components/header-footer/Footer.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import Contact from './Contact'
+import Socials from '../ui/Socials'
 
 const Footer = () => {
     return (
         <footer className="footer">
             <div className="wrapper wrapper--footer">
                 <div className="footer__logos">
-                    <div className="logo"></div>
+                    <div className="flogo"></div>
                     <div className="pro-ref-logo"></div>
-                    <div className="footer__copy">©2020 Varela Electricité - lpChamps` make</div>
+                    <div className="footer__copy">
+                        ©2020 Varela Electricité - lpChamps` make
+                    </div>
                 </div>
                 <div className="footer__contact">
                     <h3>NOUS RENCONTRER</h3>
@@ -16,6 +19,7 @@ const Footer = () => {
                 </div>
                 <div className="footer__social">
                     <h3>RESEAUX SOCIAUX</h3>
+                    <Socials />
                 </div>
             </div>
         </footer>
