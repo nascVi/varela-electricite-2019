908a7c3ec5281ca9a013b65f65e3de746f2d67a8
nascvi <vaiiho.nascimbene@gmail.com>
2020-05-25 16:40:06 +0200

    dates in parents 📆

--- src/data/varela.js
+++ src/data/varela.js
@@ -1,31 +1,31 @@
 export const varela = [
     {
-        year: 2014,
-        url: '/img/cuisine.png',
+        year: 2020,
+        url: '/img/personal-kitchen-light.png',
         title: 'Installation Romainville',
         text: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.'
     },
     {
-        year: 2015,
-        url: '/img/installavi.png',
+        year: 2018,
+        url: '/img/cuisine.png',
         title: 'FIRST FAME',
         text: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.'
     },
     {
         year: 2017,
-        url: '/img/livininstal.png',
+        url: '/img/fassade.png',
         title: 'Projet résidence Ponto Combault',
         text: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.'
     },
     {
-        year: 2019,
-        url: '/img/personal-kitchen-light.png',
+        year: 2016,
+        url: '/img/livininstal.png',
         title: 'AMENAGEMENT POUR UN RESTAURATEUR',
         text: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.'
     },
     {
-        year: 2020,
-        url: '/img/fassade.png',
+        year: 2015,
+        url: '/img/installavi.png',
         title: 'PROJET EN COURS',
         text: 'Lorem ipsummmmm sjfhjndoizje cinteqlsjirz szoeaporkof seeo,kd,z;elf, seddiams nomorning either temô sdkfnlkq sfjlkd mqlsdk zeklfj ,ceoofzmlef'
     }
