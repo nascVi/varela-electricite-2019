6797d1b673b571da6afdcc45b9045964a91283fb
Vaiiho Nascimbene <vaiihonascimbene@Vaiihos-iMac.local>
2020-11-03 03:17:32 +0100

    :package:

--- src/components/PremiereVis/PremiereVis.jsx
+++ src/components/PremiereVis/PremiereVis.jsx
@@ -1,51 +1,13 @@
-import React from 'react'
-import { boutique } from '../../data/boutique'
-import { Fade } from 'react-reveal'
-import UniteBoutique from './UniteBoutique'
+import React from 'react';
+import Post from './Post';
+import './PremiereVis.css';
 
-const PremiereVis = () => {
-    const compteursBoutique = boutique.filter(unite => unite.type === 'compteurs');
-    const cablesBoutique = boutique.filter(unite => unite.type === 'cables');
-    const kitsDomotiqueBoutique = boutique.filter(unite => unite.type === 'kitsDomotiques');
-    const coffretsBoutique = boutique.filter(unite => unite.type === 'coffrets');
-
-    const renderUnites = unitesBoutique => (
-        unitesBoutique.map((uniteBoutique, i) => <UniteBoutique {...uniteBoutique} key={i} />)
-    )
-    return (
-        <div className="wrapper">
-            <div className="boutique">
-                <h1>MATERIEL QUE NOUS RECOMMANDONS</h1>
-                <Fade>
-                    <h3>COMPTAGES & GESTION</h3>
-                    <div className="boutique__range">
-                        {renderUnites(compteursBoutique)}
-                    </div>
-                </Fade>
-                <Fade>
-                    <br />
-                    <h3>APPAREILLAGE & MODULES</h3>
-                    <div className="boutique__range">
-                        {renderUnites(cablesBoutique)}
-                    </div>
-                </Fade>
-                <Fade>
-                    <br />
-                    <h3>KITS DOMOTIQUES</h3>
-                    <div className="boutique_range">
-                        {renderUnites(kitsDomotiqueBoutique)}
-                    </div>
-                </Fade>
-                <Fade>
-                    <br />
-                    <h3>DISTRIBUTION D'ENERGIE</h3>
-                    <div className="boutique_range">
-                        {renderUnites(coffretsBoutique)}
-                    </div>
-                </Fade>
-            </div>
-        </div>
-    )
+function PremiereVis() {
+  return (
+    <div className="PremiereVis">
+      <Post />
+    </div>
+  );
 }
 
-export default PremiereVis
+export default PremiereVis;
