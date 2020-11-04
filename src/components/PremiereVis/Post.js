45c46e35e9e239a138b2b528cefb7f8cba8987d1
Vaiiho Nascimbene <vaiihonascimbene@Vaiihos-iMac.local>
2020-11-04 06:17:10 +0100

    more 1ère visite :green_heart:

--- src/components/PremiereVis/Post.js
+++ src/components/PremiereVis/Post.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import PDF from './PDF';
+import logo from './icone.png'
 
 class Post extends Component {
     state = {
@@ -38,18 +39,23 @@ class Post extends Component {
                                     <div className="well well-sm">
                                         <form className="form-horizontal" method="post">
                                             <fieldset>
-                                                <legend className="text-center header">Add new Post</legend>
+                                                <legend className="text-center header">
+                                                    Par mail: varenergie@gmail.com - 
+                                                    Vous nous contactez directement: +331 64 62 26 49 - 
+                                                    Vous souhaitez joindre le web master: lpchampigny@hotmail.com
+                                                </legend>
+                                                    <img src={ logo } width="150px" alt="logo"/>
                                                 <div className="form-group">
                                                     <span className="col-md-1 col-md-offset-2 text-center"><i className="fa fa-user bigicon"></i></span>
-                                                    <input onChange={this.onChange('title')} name="title" type="text" placeholder="Post Title" className="form-control" />
+                                                    <input onChange={this.onChange('title')} name="title" type="text" placeholder="Nom de votre Projet..." className="form-control" />
                                                 </div>
                                                 <div className="form-group">
-                                                    <span className="col-md-1 col-md-offset-2 text-center"><i className="fa fa-user bigicon"></i></span>
-                                                    <input onChange={this.onChange('image')} name="image" type="text" placeholder="https://" className="form-control" />
+                                                    <span className="col-md-1 col-md-offset-2 text-center"><i className="fa fa-user bigicon">Votre photo est sur le cloud? Ajoutez l'adresse de votre album ou la photo seule.</i></span>
+                                                    <input onChange={this.onChange('image')} name="image" type="text" placeholder="https://..." className="form-control" />
                                                 </div>
                                                 <div className="form-group">
                                                     <span className="col-md-1 col-md-offset-2 text-center"><i className="fa fa-pencil-square-o bigicon"></i></span>
-                                                    <textarea onChange={this.onChange('content')} className="form-control" name="content" placeholder="Enter your text here" rows="7"></textarea>
+                                                    <textarea onChange={this.onChange('content')} className="form-control" name="content" placeholder="Décrivez votre projet... " rows="7"></textarea>
                                                 </div>
                                                 <div className="form-group">
                                                     <button type="button" onClick={this.sunmitPost} className="btn btn-primary btn-lg">Submit</button>
--- src/components/PremiereVis/PremiereVis.jsx
+++ src/components/PremiereVis/PremiereVis.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Post from './Post';
-import './PremiereVis.css';
 
 function PremiereVis() {
   return (
