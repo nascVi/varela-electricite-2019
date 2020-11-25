cf490872d2ae3bd79e0758bcff50d487a15d3421
nascvi <vaiiho.nascimbene@gmail.com>
2020-11-25 05:00:41 +0100

    1ère Vis 📱 RES: & upgrades

--- src/components/PremiereVis/Post.js
+++ src/components/PremiereVis/Post.js
@@ -38,27 +38,43 @@ class Post extends Component {
                                 <div className="col-md-12">
                                     <div className="well well-sm">
                                         <form className="form-horizontal" method="post">
+                                            <div className="VFLogo">
+                                                <img src={ logo } width="150px" alt="logo"/>
+                                            </div>
                                             <fieldset>
-                                                <legend className="text-center header">
-                                                    Par mail: varenergie@gmail.com - 
-                                                    Vous nous contactez directement: +331 64 62 26 49 - 
-                                                    Vous souhaitez joindre le web master: lpchampigny@hotmail.com
-                                                </legend>
-                                                    <img src={ logo } width="150px" alt="logo"/>
+                                                <p className="text-center header">
+                                                    Par mail: 
+                                                        <a href="mailto:varenergie@gmail.com">
+                                                        varenergie@gmail.com
+                                                        </a>
+                                                        <br/> 
+                                                    Ligne directe: 
+                                                        <a href="tel:+33164622649">
+                                                        +331 64 62 26 49 
+                                                        </a>
+                                                        <br/>
+                                                    io: <a href="mailto:lpchampigny@hotmail.com">
+                                                            lpChamps`
+                                                        </a>
+                                                </p>
                                                 <div className="form-group">
                                                     <span className="col-md-1 col-md-offset-2 text-center"><i className="fa fa-user bigicon"></i></span>
                                                     <input onChange={this.onChange('title')} name="title" type="text" placeholder="Nom de votre Projet..." className="form-control" />
                                                 </div>
                                                 <div className="form-group">
-                                                    <span className="col-md-1 col-md-offset-2 text-center"><i className="fa fa-user bigicon">Votre photo est sur le cloud? Ajoutez l'adresse de votre album ou la photo seule.</i></span>
+                                                    <span className="col-md-1 col-md-offset-2 text-center"><i className="fa fa-user bigicon">Ajouter une photo avec l'url.</i></span>
                                                     <input onChange={this.onChange('image')} name="image" type="text" placeholder="https://..." className="form-control" />
+                                                </div>
+                                                <div className="form-group">
+                                                    <span className="col-md-1 col-md-offset-2 text-center"><i className="fa fa-user bigicon">Photo du bureau.</i></span>
+                                                    <input onChange={this.onChange('file')} name="file" type="file" className="form-control" />
                                                 </div>
                                                 <div className="form-group">
                                                     <span className="col-md-1 col-md-offset-2 text-center"><i className="fa fa-pencil-square-o bigicon"></i></span>
                                                     <textarea onChange={this.onChange('content')} className="form-control" name="content" placeholder="Décrivez votre projet... " rows="7"></textarea>
                                                 </div>
                                                 <div className="form-group">
-                                                    <button type="button" onClick={this.sunmitPost} className="btn btn-primary btn-lg">Submit</button>
+                                                    <button type="button" onClick={this.sunmitPost} className="btn btn-primary btn-lg">Carbone</button>
                                                 </div>
                                             </fieldset>
                                         </form>
