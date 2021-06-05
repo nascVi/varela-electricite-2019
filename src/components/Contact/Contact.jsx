58188e2e6f1522d4f166da9b57e9dfddb1dbf95d
nascvi <vaiiho.nascimbene@gmail.com>
2021-06-05 19:59:17 +0200

    form contact 📭

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,20 +1,36 @@
-import React from 'react';
-import Post from './Post';
+import React from "react";
+import { useForm } from "react-hook-form";
+
+import "./Contact.css";
 
 
-const deskUpload = () => {
+const Contact = () => {
+  const { register, handleSubmit, watch, errors } = useForm();
+  const onSubmit = data => console.log(data);
+  const message = watch('message') || "";
+  const messageCharsLeft = 1500 - message.length;
+  
   return (
-    <form>
-      <input type="file"/>
-    </form>
-  )
-}
+  <div>
+      <form id='contact-form' onSubmit={handleSubmit(onSubmit)}>
+      <h3>Prendre Contact</h3>
+      <br/>
+      <br/>
+        <input type='text' name='user_name' placeholder='Nom' {...register('text')} maxLength='30' />
+        <br/>
+        <input type='email' name='user_email' placeholder='Email' {...register('email')} maxLength='30' />
+        <br/>
+        <input type='text' name='user_subject' placeholder='Objet' {...register('text')} maxLength='30'/>
+        <br/>
+        <input type='text' name='user_phone' placeholder='📞 Numéro de téléphone' {...register('text')} maxLength='30'/>
+        <br/>
+        <textarea name='message' placeholder='Votre message' {...register('message')} maxLength='1500' />
+        <br />
+        <p className='message-chars-left'>{messageCharsLeft} caracters</p>
 
-function Contact() {
-  return (
-    <div className="contact">
-      <Post />
-    </div>
+        <input type='submit' value='Envoyer' />
+      </form>
+  </div>
   );
 }
 
